8ac95df1db4e29b5e56a76ebe47f16bb247fa0b2
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-01-02 17:40:10 -0500

    worked on main page CSS

--- questions.js
+++ questions.js
@@ -1,28 +1,58 @@
 var questions = [
   {
-    title: "Question 1",
-    options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer: "Answer 2",
+    title:
+      "What is the difference between double equals (==) and triple equals (===) operators?",
+    options: [
+      "Double equals (==) is an array, while triple equals (===) is a string.",
+      "Double equals (==) is an equality operator to compare data for similarities, while triple equals (===) looks for strict equality if data is the same or not.",
+      "Double equals (==) performs logical operations, while triple equals (===) uses loops to process arrays.",
+      "Double equals (==) looks for true equality variables if data is the same or not, while triple equals (===) is an array to compare false data similarities.",
+    ],
+    answer:
+      // Answer 2
+      "Double equals (==) is an equality operator to compare data for similarities, while triple equals (===) looks for strict equality if data is the same or not.",
   },
   {
-    title: "Question 2",
-    options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer: "Answer 3",
+    title: "What are callbacks?",
+    options: [
+      "A call back function is also known as the DOM.",
+      "A callback function is a reoccurring function that gets recalled every-time the function is performed.",
+      "A callback function is a function that will be executed after another function gets executed.",
+      "A callback function provides us with a new and shorter syntax for declaring functions.",
+    ],
+    answer:
+      "A callback function is a function that will be executed after another function gets executed.",
   },
   {
-    title: "Question 3",
-    options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer: "Answer 1",
+    title: "What are the three primative data types used in Javascript?",
+    options: [
+      "Boolean, strings, and numbers.",
+      "Strings, alphabet, and numbers.",
+      "Numbers, alphabet, and booleans.",
+      "Strings, booleans, numbers, and alphabet.",
+    ],
+    answer: "Boolean, strings, and numbers.",
   },
   {
-    title: "Question 4",
-    options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer: "Answer 2",
+    title: "What is a function expression?",
+    options: [
+      "A function expression is expressed through multiple variables.",
+      "When a function is created and assigned it to a variable.",
+      "Creating a function and using the keyword first.",
+      "A function expression is when you execute an  ",
+    ],
+    answer: "When a function is created and assigned it to a variable.",
   },
   {
-    title: "Question 5",
-    options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer: "Answer 4",
+    title: "What is the use of promises in Javascript?",
+    options: [
+      "Promises provide a new way of declaring constructor functions in javascript.",
+      "Promises are never meant to be broken which in turn always equals true.",
+      "Promises are declared with the function keyword instead of the normal function.",
+      "Promises are used to handle asynchronous operations in Javascript.",
+    ],
+    answer:
+      "Promises are used to handle asynchronous operations in Javascript.",
   },
 ];
 
--- script.js
+++ script.js
@@ -9,7 +9,6 @@ var startQuizButton = document.getElementById("start-quiz");
 var questionsContainer = document.getElementById("questions");
 var questionTitle = document.getElementById("question-title");
 var questionsList = document.getElementById("options");
-var nextButton = document.getElementById("next-button");
 
 // Give our Start Quiz Button an onClick event
 startQuizButton.addEventListener("click", function (event) {
@@ -64,3 +63,25 @@ function checkAnswer(usersChoice, answer) {
     return false;
   }
 }
+
+//Tina's Code
+
+var timer = 60;
+var counter = 0;
+var currentScore = 0;
+var intro = document.getElementById("IntroContainer");
+var game = document.getElementById("GameContainer");
+console.log("loadIt");
+var questions = [
+  {
+    Question: "Commonly used data types do not include..",
+    Answers: ["strings", "Boleans", "Numbers", "Alerts"],
+    Correct: "Alerts",
+  },
+];
+function StartGame() {
+  console.log("buttonClicked");
+  into.classList.add("Hide");
+  Game.classList.remove("Hide");
+}
+document.getElementById("StartButton").addEventListener("click", StartGame);
